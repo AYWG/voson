3d74e86a1fcd252a75913baaf525ce9b055148c6
Andy Wong <andygbwong@gmail.com>
2019-02-23 11:30:28 -0800

    Added other modes (chill, sport, track)

--- src/common.c
+++ src/common.c
@@ -2,8 +2,14 @@
 // Common Menu Stuff
 ////////////////////////////////////////////////////////////////////////////////
 #define DOWN_ARROW_START_Y 18
-#define ECO_BAR_START_X 0
-#define ECO_BAR_START_Y 22
+#define CHILL_BAR_START_X 0
+#define CHILL_BAR_START_Y 22
+#define ECO_BAR_START_X CHILL_BAR_START_X
+#define ECO_BAR_START_Y CHILL_BAR_START_Y
+#define SPORT_BAR_START_X CHILL_BAR_START_X
+#define SPORT_BAR_START_Y CHILL_BAR_START_Y
+#define TRACK_BAR_START_X CHILL_BAR_START_X
+#define TRACK_BAR_START_Y CHILL_BAR_START_Y
 #define MENU_BAR_START_X 0
 #define MENU_BAR_START_Y 20
 
@@ -24,10 +30,24 @@ void common_draw_menu_arrow(void)
     oled_draw(down_arrow, sizeof down_arrow, 52, DOWN_ARROW_START_Y, DOWN_ARROW_WIDTH);
 }
 
-// TODO: Handle other types (not just eco, assuming there are others??)
+void common_draw_chill_bar(void)
+{
+    oled_draw(chill_bar, sizeof chill_bar, CHILL_BAR_START_X, CHILL_BAR_START_Y, CHILL_BAR_WIDTH);
+}
+
 void common_draw_eco_bar(void)
 {
     oled_draw(eco_bar, sizeof eco_bar, ECO_BAR_START_X, ECO_BAR_START_Y, ECO_BAR_WIDTH);
+}
+
+void common_draw_sport_bar(void)
+{
+    oled_draw(sport_bar, sizeof sport_bar, SPORT_BAR_START_X, SPORT_BAR_START_Y, SPORT_BAR_WIDTH);
+}
+
+void common_draw_track_bar(void)
+{
+    oled_draw(track_bar, sizeof track_bar, TRACK_BAR_START_X, TRACK_BAR_START_Y, TRACK_BAR_WIDTH);
 }
 
 void common_draw_menu_bar(void)
